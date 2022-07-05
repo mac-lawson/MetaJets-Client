f18697cc0ade1bb4869c915f80fc7cba4a422484
Mac L <macattackairman@gmail.com>
2022-07-05 15:43:01 -0400

    worked on pychache

--- interface/client.py
+++ interface/client.py
@@ -5,18 +5,24 @@ from clientStyle import coloring
 from interface import run   
 from system import uptime
 from time import sleep
+import sys
 
 
 def mainMenu():
     while True:
         print((coloring.terminal.fail) + (logo.logo) + (coloring.terminal.end))
         menuOptions = input('Press Enter to Run')
+
+
         print((coloring.terminal.green) + 'pinging localhost... ' + (coloring.terminal.end))
         sleep(3)
         uptime.ping('127.0.0.1')
         print((coloring.terminal.green) + 'cURL localhost... ' + (coloring.terminal.end))
         sleep(3)
         uptime.curl('127.0.0.1:80')
+        print((coloring.terminal.green) + 'request GET to portal... ' + (coloring.terminal.end))
+        sleep(3)
+        uptime.get('http://metajets.tk')
 
 
 
--- system/uptime.py
+++ system/uptime.py
@@ -1,8 +1,14 @@
 import os
 from time import time
+from urllib import response
+import requests
 
 def ping(ip):
     os.system('ping ' + (ip) + ' -t 5')
 
 def curl(url):
     os.system('curl ' + (url))    
+
+def get(www):
+    response = requests.get(www)
+    print(response)
